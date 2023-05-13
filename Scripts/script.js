7acdba68952ae8c52f4359fd8f29e2f0fd80dc06
Marcio <mibfiap@outlook.com>
2023-05-12 22:48:48 -0300

    RM98811

--- Scripts/script.js
+++ Scripts/script.js
@@ -4,8 +4,66 @@ const switchBtn = document.getElementById('switch');
 switchBtn.addEventListener('click', () => {
   document.body.classList.toggle('dark-theme');
   document.querySelector('#Principal').classList.toggle('dark-theme');
-  document.querySelector('.paragrafo').classList.toggle('dark-theme');
 });
+
+if (document.body.id == 'Quiz') { // alerta que quando entrar no Quiz seja executada
+  alert('Vamos conhecer Curiosidades!! ');
+  var inicio_quiz = prompt("Você quer participar do Quiz? \nDigite sim ou nao")
+}
+
+let resultado = 0;
+
+if (inicio_quiz == "sim") {
+  var resposta1 = prompt("Pergunta 1: Qual o material mais comum utilizado para fazer perucas? \nA) Cabelo humano \nB) Cabelo animal\nC) Cabelo sintético");
+
+  if (resposta1 !== null) {
+      resposta1 = resposta1.toLowerCase();
+    
+      if (resposta1 === "c") {
+        resultado++;
+      }
+  }
+
+  var resposta2 = prompt("Pergunta 2: Qual a principal causa de perda de cabelo em homens? \nA) Estresse \nB) Hereditariedade \nC) Má alimentação ");
+  if (resposta2 !== null) {
+    resposta2 = resposta2.toLowerCase();
+
+    if (resposta2 === "b") {
+      resultado++;
+    }
+  }
+
+  var resposta3 = prompt("Pergunta 3: Qual é a melhor maneira de limpar e cuidar de uma peruca? \nA) Lavá-la com água quente e sabão. \nB) Deixá-la de molho em água com vinagre. \nC) Usar um spray de limpeza especializado e escovar suavemente. ");
+
+  if (resposta3 !== null) {
+      resposta3= resposta3.toLowerCase();
+    
+      if (resposta3 === "c") {
+        resultado++;
+      }
+  }
+
+  if (resultado == 1){
+    const mensagem = document.querySelector("#resultados");
+    mensagem.innerHTML = "Você acertou uma pergunta!";
+  }
+  else if (resultado == 2){
+    const mensagem = document.querySelector("#resultados");
+    mensagem.innerHTML = "Muito bem, você acertou duas perguntas!";
+  }
+  else if (resultado == 3){
+    const mensagem = document.querySelector("#resultados");
+    mensagem.innerHTML = "Três acertos! Parabéns!";
+  }
+  
+  else{
+    const mensagem = document.querySelector("#resultados");
+    mensagem.innerHTML = "Zero acertos.";
+  }
+}else{
+  const mensagem = document.querySelector("#resultados");
+  mensagem.innerHTML = "Que tal na próxima?";
+}
 
 
 
